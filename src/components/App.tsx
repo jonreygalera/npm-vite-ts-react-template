672207ea92da31d9f814fbb75ddd04d4067e5b9c
Jon Rey Galera <jrg@newmediastaff.com>
2023-04-21 21:10:11 +0800

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+
+}
+
+const App: React.FC<Props> = (props) => {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'npm-vite-ts-react-template',
+      formats: ['es', 'umd'],
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  }
+})
